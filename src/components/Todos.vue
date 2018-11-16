ff30ca073ddb95d51162c7afd9303950c062b718
bdurb <brian@rpmgreenville.com>
2018-11-15 17:27:27 -0800

    working with vue directives v-if and v-else

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,8 @@
       <ul>
         <li v-for="(data, index) in todos" :key="index">{{ index }}. {{ data.todo }}</li>
       </ul>
+      <p v-if="todos.length >= 1">You have more than 1 todo</p>
+      <p v-else>You have less than or equal to 1 todo</p>
     </div>
   </div>
 </template>
