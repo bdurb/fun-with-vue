f9e4ba1d0ebb2500091df8bb13bd0ef6ab1b123c
bdurb <brian@rpmgreenville.com>
2018-11-15 19:09:22 -0800

    added about component, set up router, and commented out todos import into app.vue as its not necessary with the router setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <Todos />
+    <nav>
+      <router-link to='/'>Home</router-link>
+      <router-link to='/about'>About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+// import Todos from './components/Todos.vue'
 
 export default {
   name: 'app',
-  components: {
-    Todos
-  }
+  // components: {
+  //   Todos
+  // }
 }
 </script>
 
@@ -24,7 +29,7 @@ body {
   display: grid;
   grid-template-rows: auto;
   justify-items: center;
-  align-items: center;
+  padding-top: 50px;
 }
 body, html {
   margin: 0;
@@ -32,5 +37,19 @@ body, html {
 }
 #app {
     width: 50%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
 }
 </style>
