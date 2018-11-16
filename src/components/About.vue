55d4e88e29ab08ef2e4f32d4d9318ababc2d9d82
bdurb <brian@rpmgreenville.com>
2018-11-15 19:24:53 -0800

    added some content to the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an About page</h1>
-    <p>Stuff and other things and Junk</p>
+    <h1>Todo or not Todo, that is the question</h1>
+    <p>This application was a little into into Vue.  Having worked almost exclusively with 
+      React, or Vanilla JS, it was fun to see the difference in layout, and setup that Vue
+      offers, vs React.  Now Add some Todos, and Todo them. 
+    </p>
   </div>
 </template>
