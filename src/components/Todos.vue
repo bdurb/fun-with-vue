e05822a22ff13934f21527ade94b19adcf6dcc54
Sumayyah Asgar <sasgar04@gmail.com>
2018-12-06 18:23:53 -0800

    notes added 📝

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,18 +3,25 @@
     <div class="holder">
 
       <form @submit.prevent="addTodo">
+      <!-- similar to prevent default in react
+       uses the addTodo method we have in our script tags below-->
       <input type="text" placeholder="Enter a Todo.." v-model="todo" v-validate="'min:5'" name="todo">
+      <!-- v-model used for two way binding of input to the todo from data -->
 
       <transition name="alert-in">
         <p class="alert" v-if="errors.has('todo')">{{ errors.first('todo') }}</p>
+        <!-- v-if is an if statement 
+        {{}} indicates javascript -->
       </transition>
       </form>
 
       <ul>
         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
           <li v-for="(data, index) in todos" :key="index">
+          <!-- v-for is the same as a for in loop (like mapping) -->
             {{ data.todo }}
             <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+            <!-- v-on:click same as onClick() method -->
           </li>
         </transition-group>
       </ul>
@@ -52,6 +59,12 @@ export default {
   }
 }
 </script>
+
+  <!-- 
+  name of the script would be the componet you are exporting
+  data is similar to state in react
+  reset the state by setting todo to an empty string every time after you add a new todo
+  -->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -130,3 +143,7 @@ export default {
     cursor: pointer;
   }
 </style>
+
+<!-- template tag is your html -->
+<!-- script tag is your javascript -->
+<!-- style tag is your css -->
